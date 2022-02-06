adbe1412641daceebfd0aa602acb10b17c318be1
Chintamani Raju Virulkar <chintamani.virulkar@gmail.com>
2022-02-06 19:39:41 +0530

    update

--- app.py
+++ app.py
@@ -3,43 +3,30 @@ from tensorflow import keras
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 import numpy as np
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/prediction', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     imagefile = request.files['imagefile']
-    image_path = "./images/" + imagefile.filename
+    image_path = "D:/Brain-Tumor-Detection/static/uploaded_images/" + imagefile.filename
     imagefile.save(image_path)
 
     model = keras.models.load_model("model.h5")
     img = load_img(image_path, target_size=(150,150))
     x = img_to_array(img)
-    plt.imshow(x)
     x = np.expand_dims(x, axis=0)
     images = np.vstack([x])
     pred = model.predict(images)
     pred = int(pred)
 
-    return render_template("result.html", path=image_path, pred=pred)
+    return render_template("result.html", pred=pred)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
+if __name__ == "__main__" :
     app.run(debug=True)
